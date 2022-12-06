ec281a595a3bbd7c373aa7b6343259795bf6095b
Nick Hounshell <nhounshell@community.com>
2022-12-06 13:53:42 -0500

    Don't love my abstraction, but 2.2

--- day_2.py
+++ day_2.py
@@ -13,25 +13,62 @@ SHAPE_POINTS = {
     'SCISSORS': 3
 }
 
+RESULTS = {
+    'X': 'LOSE',
+    'Y': 'DRAW',
+    'Z': 'WIN'
+}
+
 
 class Shape:
-    def __init__(self, letter: str):
-        self.shape = LETTER_MATCH[letter]
+    def __init__(self, letter: str = None, name: str = None):
+        if letter:
+            self.shape = LETTER_MATCH[letter]
+        elif name:
+            self.shape = name
+
         self.points = SHAPE_POINTS[self.shape]
 
-    def resolve(self, other):
-        if self.shape == other.shape:
+    def beats(self):
+        return {
+            'ROCK': 'SCISSORS',
+            'SCISSORS': 'PAPER',
+            'PAPER': 'ROCK'
+        }[self.shape]
+
+    def loses_to(self):
+        return {
+            'SCISSORS': 'ROCK',
+            'PAPER': 'SCISSORS',
+            'ROCK': 'PAPER'
+        }[self.shape]
+
+
+class Match:
+    def __init__(self, us: Shape, them: Shape):
+        self.us = us
+        self.them = them
+
+    def resolve(self):
+        if self.us.shape == self.them.shape:
             return 3
-        elif self.shape == 'ROCK':
-            return 6 if other.shape == 'SCISSORS' else 0
-        elif self.shape == 'PAPER':
-            return 6 if other.shape == 'ROCK' else 0
+        elif self.them.shape == self.us.beats():
+            return 6
         else:
-            return 6 if other.shape == 'PAPER' else 0
+            return 0
 
-    def score(self, other):
-        return self.resolve(other) + self.points
+    def score(self):
+        return self.resolve() + self.us.points
 
+
+def fix_the_match(opp_shape: Shape, result_key: str) -> Shape:
+    result = RESULTS[result_key]
+    if result == 'WIN':
+        return Shape(name=opp_shape.loses_to())
+    elif result == 'LOSE':
+        return Shape(name=opp_shape.beats())
+    else:
+        return Shape(name=opp_shape.shape)
 
 
 def main(fpath: str):
@@ -39,12 +76,13 @@ def main(fpath: str):
 
     with open(fpath) as f:
         for line in f:
-            them, us = line.strip().split()
+            them, result_key = line.strip().split()
 
-            them = Shape(them)
-            us = Shape(us)
+            them = Shape(letter=them)
 
-            scores.append(us.score(them))
+            us = fix_the_match(them, result_key)
+
+            scores.append(Match(us, them).score())
 
     return sum(scores)
 
