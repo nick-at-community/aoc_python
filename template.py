7f748199d2e53cd2d25a8fcc4e16368c95128510
Nick Hounshell <nhounshell@community.com>
2022-12-09 20:48:34 -0500

    Add dummy template file

---
+++ template.py
@@ -0,0 +1,19 @@
+
+
+def part_one():
+    with open('input.txt') as f:
+        data = f.read()
+
+def part_two():
+    with open('input.txt') as f:
+        data = f.read()
+
+
+
+def main():
+    print(part_one())
+    print(part_two())
+
+
+if __name__ == '__main__':
+    main()
