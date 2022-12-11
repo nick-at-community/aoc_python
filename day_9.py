b78f8b52f73f0db8a22426582c1830ea82da86f6
Nick Hounshell <nhounshell@community.com>
2022-12-11 16:03:04 -0500

    Generalize part 9.1 in anticipation of .2

--- day_9.py
+++ day_9.py
@@ -9,41 +9,64 @@ def dist(head, tail):
 
 
 def move_head(dir):
-    global head_pos
+    global snake
 
     if dir == 'R':
-        head_pos = [head_pos[0] + 1, head_pos[1]]
+        snake[0] = [snake[0][0] + 1, snake[0][1]]
     elif dir == 'L':
-        head_pos = [head_pos[0] - 1, head_pos[1]]
+        snake[0] = [snake[0][0] - 1, snake[0][1]]
     elif dir == 'U':
-        head_pos = [head_pos[0], head_pos[1] + 1]
+        snake[0] = [snake[0][0], snake[0][1] + 1]
     else:
-        head_pos = [head_pos[0], head_pos[1] - 1]
+        snake[0] = [snake[0][0], snake[0][1] - 1]
 
-def move_tail():
-    global head_pos, tail_pos, last_head_pos, tail_visited
+def move_tail(tail_idx: int):
+    global snake, last_head_pos, tail_visited
 
-    d = dist(head_pos, tail_pos)
+    d = dist(snake[0], snake[tail_idx])
     if d <= 1:
         pass
     elif d == sqrt(2): # diag is still legal
         pass
     elif d == 2: # straight line follow
-        tail_pos = last_head_pos
+        snake[tail_idx] = last_head_pos
     else: # catch up diag move
-        tail_pos = last_head_pos
+        snake[tail_idx] = last_head_pos
 
-    tail_visited.add(tuple(tail_pos))
+    tail_visited.add(tuple(snake[tail_idx]))
 
 
 def part_one(fpath: str):
+    global snake, last_head_pos, tail_visited
+
+    with open(fpath) as f:
+        data = f.read()
+
+    tail_idx = 1
+    tail_visited = set()
+    snake = {x: [0, 0] for x in range(2)}
+
+    for step in data.splitlines():
+        dir, amt = step.split()
+        print(dir, amt)
+
+        amt = int(amt)
+        for tick in range(amt):
+            last_head_pos = snake[0]
+            move_head(dir)
+            move_tail(tail_idx)
+            print(snake[0], snake[1])
+
+    return len(tail_visited)
+
+def part_two(fpath: str):
     global head_pos, last_head_pos, tail_pos, tail_visited
 
     with open(fpath) as f:
         data = f.read()
 
     tail_visited = set()
-    head_pos = [0, 0]
+
     tail_pos = [0, 0]
 
     for step in data.splitlines():
@@ -59,17 +82,13 @@ def part_one(fpath: str):
 
     return len(tail_visited)
 
-def part_two(fpath: str):
-    with open(fpath) as f:
-        data = f.read()
-
 
 
 def main(fpath: str):
     print(part_one(fpath))
-    print(part_two(fpath))
+    # print(part_two(fpath))
 
 
 if __name__ == '__main__':
     main('prompt.txt')
-    main('input.txt')
+    # main('input.txt')
