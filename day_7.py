0962910152b269713660fdf79ddbb3a5c7f141d5
Nick Hounshell <nhounshell@community.com>
2022-12-11 14:03:45 -0500

    Woof.

---
+++ day_7.py
@@ -0,0 +1,73 @@
+import os
+from typing import Dict, Set, Tuple
+
+def build_files_and_dirs() -> Tuple[Dict[str, int], Set]:
+    with open('input.txt') as f:
+        data = f.read()
+
+    files = {}
+    dirs = set(['/'])
+
+    pwd = '/'
+    in_ls = False
+    for line in data.splitlines()[1:]:
+        if in_ls and line.startswith('$'):
+            in_ls = False
+        elif in_ls and line.startswith('dir '):
+            _, dirname = line.split(' ', 1)
+            dirs.add(os.path.join(pwd, dirname))
+            continue
+        elif in_ls:
+            sz, filename = line.split(' ', 1)
+            files[os.path.join(pwd, filename)] = int(sz)
+            continue
+
+        if line == '$ ls':
+            in_ls = True
+        elif line == '$ cd ..':
+            pwd = os.path.dirname(pwd) or '/'
+        elif line.startswith('$ cd'):
+            pwd = os.path.join(pwd, line.split(' ', 2)[-1])
+        else:
+            raise AssertionError(line)
+
+    return files, dirs
+
+def calc_dirsize(dirname: str):
+    size = 0
+    for k, v in files.items():
+        if k.startswith(dirname):
+            size += files[k]
+
+    return size
+
+def part_one():
+    global files, dirs
+    files, dirs = build_files_and_dirs()
+
+    sizes = [
+        calc_dirsize(dirname)
+        for dirname
+        in dirs
+    ]
+
+    return sum(
+        size
+        for size
+        in sizes
+        if size <= 100000
+    )
+
+def part_two():
+    with open('input.txt') as f:
+        data = f.read()
+
+
+
+def main():
+    print(part_one())
+    print(part_two())
+
+
+if __name__ == '__main__':
+    print(part_one())
