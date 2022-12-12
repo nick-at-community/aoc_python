86207e42aecee43a7e7b3bdf82b328c1f8db30cd
Nick Hounshell <nhounshell@community.com>
2022-12-12 14:15:24 -0500

    No more off-by-one errors :')

--- day_10.py
+++ day_10.py
@@ -1,6 +1,7 @@
 from itertools import islice
+import numpy as np
 
-
+np.set_printoptions(linewidth=180)
 
 def part_one(fpath: str):
     with open(fpath) as f:
@@ -38,13 +39,59 @@ def part_one(fpath: str):
 
 ############
 
+def make_crt(output = np.array):
+    return output.reshape((6, 40))
 
 
+def print_crt(crt: np.array):
+    for line in crt:
+        print(''.join(line))
+
+
+class X:
+    def __init__(self, val: int = 1):
+        self.val = val
+
+    def __add__(self, other):
+        return type(self)(self.val + other)
+
+    @property
+    def sprite_trio(self):
+        return (self.val - 1, self.val, self.val + 1)
+
+    def print_pixel(self, loc: int):
+        return loc % 40 in self.sprite_trio
 
 def part_two(fpath: str):
     with open(fpath) as f:
         data = f.read()
 
+    cycle = 0
+    x = X()
+
+    output = []
+    for line in data.splitlines():
+        # print(line)
+        try:
+            command, val = line.split()
+            for i in range(2):
+                # print(cycle, x.val)
+                char = '#' if x.print_pixel(cycle) else '.'
+                output.append(char)
+                cycle += 1
+            else:
+                x += int(val)
+
+        except ValueError:
+            print(cycle, x.val)
+            char = '#' if x.print_pixel(cycle) else '.'
+            output.append(char)
+            cycle += 1
+
+        # print(x.sprite_trio)
+
+    crt = make_crt(np.array(output))
+    print_crt(crt)
 
 
 def main(fpath: str):
